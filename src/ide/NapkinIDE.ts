2c4907f4a6ff7b049efeb509cf666b7f727491b8
mcgear <michael.gearhardt@gmail.com>
2021-09-17 15:35:20 -0600

    Adjusted NapkinIDE file

--- src/ide/NapkinIDE.ts
+++ src/ide/NapkinIDE.ts
@@ -1,57 +1,20 @@
 import { NapkinFlow } from '../flow/NapkinFlow';
-function SJSElement<T extends { new (...args: any[]): any }>(constructor: T) {
-  return class extends constructor {
-      //  Fields
-      protected mountedEl?: HTMLElement;
+import { SJSElement, Status } from '@semanticjs/common';
 
-      //  API Methods
-      public Mount(selector: string | HTMLElement): void {
-          if (typeof selector === "string") {
-              selector = <HTMLElement>(
-                  document.querySelector(<string>selector)
-              );
-          }
-
-          this.mountedEl = this.mountToElement(<HTMLElement>selector);
-      }
-
-      //  Helpers
-      protected mountToElement(el: HTMLElement): HTMLElement {
-          if (super.mountToElement) {
-              return super.mountToElement(el);
-          } else {
-              return el;
-          }
-      }
-  };
-}
-
-@SJSElement
-export class NapkinIDE {
+export class NapkinIDE extends SJSElement {
   //  Fields
 
   //  Properties
   public Flow?: NapkinFlow;
 
+  public Status?: Status;
+
   //  Constructors
   constructor() {
-    // this.Example = "Hello World";
+    super();
   }
 
   //  API Methods
-  public ChangeExample(example: string): void {
-    // this.Example = example;
-  }
 
   //  Helpers
-  protected mountToElement(el: HTMLElement) {
-    // doesn't actually attach to the DOM right now
-    document.createDocumentFragment
-
-    //setting up the canvas
-  
-    // el.appendChild
-    // this.example = !this.example;
-  }
 }
-    
