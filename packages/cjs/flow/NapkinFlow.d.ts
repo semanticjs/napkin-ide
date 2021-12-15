150312a2375d76412dbc343d70e3f3be39da6d3b
xchanin <shannonwbruns@gmail.com>
2021-12-15 09:56:53 -0700

    upgrade semanticjs common version

---
+++ packages/cjs/flow/NapkinFlow.d.ts
@@ -0,0 +1,8 @@
+import { NapkinEdge } from "./NapkinEdge.js";
+import { NapkinNode } from "./NapkinNode.js";
+export declare class NapkinFlow {
+    Edges?: NapkinEdge[];
+    ID?: string;
+    Nodes?: NapkinNode[];
+    constructor();
+}
---
+++ packages/cjs/flow/NapkinFlow.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NapkinFlow = void 0;
+class NapkinFlow {
+    //  Constructors
+    constructor() {
+    }
+}
+exports.NapkinFlow = NapkinFlow;
